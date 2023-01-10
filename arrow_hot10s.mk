ca44e478ee208ed7fd98025a0a190fef7d12d1c6
xoo2001 <ekora2001@gmail.com>
2023-01-10 10:49:53 +0000

    hot10s: fix derp

--- arrow_hot10s.mk
+++ arrow_hot10s.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 IS_PHONE := true
 
 #
@@ -34,7 +34,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hot10s
-PRODUCT_NAME := lineage_hot10s
+PRODUCT_NAME := arrow_hot10s
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Hot 10S
 PRODUCT_MANUFACTURER := Infinix
