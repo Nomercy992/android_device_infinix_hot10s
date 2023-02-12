5ac36c0ac02317950e30bee9900b2a0930e3006f
Nomercy992 <108360182+Nomercy992@users.noreply.github.com>
2023-02-13 06:42:39 +0700

    Update arrow_hot10s.mk

--- arrow_hot10s.mk
+++ arrow_hot10s.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 DEVICE_MAINTAINER := Nomercy992
 IS_PHONE := true
 WITH_GAPPS := true
