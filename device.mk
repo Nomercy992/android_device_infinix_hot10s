1b2101ed1ada4d72e2e179ff93bf71fe698a13d2
Nomercy992 <nothacker697@gmail.com>
2023-02-15 06:03:09 +0700

    add mtk pemission

--- device.mk
+++ device.mk
@@ -94,6 +94,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend \
     android.hardware.health@2.0
+    
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
 
 # RcsService
 PRODUCT_PACKAGES += \
@@ -115,6 +125,9 @@ PRODUCT_COPY_FILES += \
 # APN's
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+    
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
 # [DNM] Temp permissions
 PRODUCT_COPY_FILES += \
